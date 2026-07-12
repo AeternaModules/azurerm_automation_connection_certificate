973cba0488f7688ca3fa8e9e4532e820c6f9e3f0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "automation_connection_certificates" {
-  description = "All automation_connection_certificate resources"
-  value       = azurerm_automation_connection_certificate.automation_connection_certificates
-}
 output "automation_connection_certificates_automation_account_name" {
-  description = "List of automation_account_name values across all automation_connection_certificates"
-  value       = [for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
+  value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.automation_account_name }
 }
 output "automation_connection_certificates_automation_certificate_name" {
-  description = "List of automation_certificate_name values across all automation_connection_certificates"
-  value       = [for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : v.automation_certificate_name]
+  description = "Map of automation_certificate_name values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
+  value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.automation_certificate_name }
 }
 output "automation_connection_certificates_description" {
-  description = "List of description values across all automation_connection_certificates"
-  value       = [for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : v.description]
+  description = "Map of description values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
+  value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.description }
 }
 output "automation_connection_certificates_name" {
-  description = "List of name values across all automation_connection_certificates"
-  value       = [for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : v.name]
+  description = "Map of name values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
+  value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.name }
 }
 output "automation_connection_certificates_resource_group_name" {
-  description = "List of resource_group_name values across all automation_connection_certificates"
-  value       = [for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
+  value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.resource_group_name }
 }
 output "automation_connection_certificates_subscription_id" {
-  description = "List of subscription_id values across all automation_connection_certificates"
-  value       = [for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : v.subscription_id]
+  description = "Map of subscription_id values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
+  value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.subscription_id }
 }
 
