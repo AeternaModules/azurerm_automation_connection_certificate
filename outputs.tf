adaf0ab0ebb2a3206f5162d0aac4042b2981dfe3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "automation_connection_certificates_id" {
+  description = "Map of id values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
+  value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.id }
+}
 output "automation_connection_certificates_automation_account_name" {
   description = "Map of automation_account_name values across all automation_connection_certificates, keyed the same as var.automation_connection_certificates"
   value       = { for k, v in azurerm_automation_connection_certificate.automation_connection_certificates : k => v.automation_account_name }
